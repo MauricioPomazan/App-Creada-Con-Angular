98a0f2164278d5c95be222f8b6a86f7d9a1e83e1
pomazan19 <poma205@gmail.com>
2022-09-19 13:54:03 -0300

    0.1

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './components/main/main.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    MainComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule 
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/main/main.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl  } from '@angular/forms';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css']
+})
+export class MainComponent implements OnInit {
+  serverElements = [
+    { nombre: 'Botines', precio: 9000, marca: 'Nike', id: 1, categoria: '3' },
+    { nombre: 'Remera PSG', precio: 50000, marca: 'Jordan', id: 1, categoria: '5' },
+    { nombre: 'Pelota Futbol', precio: 5000, marca: 'Adidas', id: 1, categoria: '6' },
+    { nombre: 'Short de futbol', precio: 2500, marca: 'Nike', id: 1, categoria: '12' },
+    { nombre: 'Medias de futbol', precio: 1000, marca: 'Nike', id: 1, categoria: '8' }
+  ];
+  listaMostrar: Array<any> = []
+
+  filterForm: FormGroup;
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.filterForm = new FormGroup({
+      id: new FormControl(''),
+      nombre: new FormControl(''),
+      precio: new FormControl(''),
+      categoria: new FormControl(''),
+      marca: new FormControl('')
+    })
+    this.listaMostrar = [...this.serverElements]
+    this.filterForm.valueChanges.subscribe(() => {
+      this.filterValues();
+    });
+  }
+
+  filterValues() {
+    this.listaMostrar = this.serverElements.filter(e => {
+      return (e.nombre.toLowerCase().includes(this.filterForm.value.nombre.toLowerCase()) &&
+        e.marca.toLowerCase().includes(this.filterForm.value.marca.toLowerCase()) &&
+        e.categoria.toLowerCase().includes(this.filterForm.value.categoria.toLowerCase())
+      )
+    })
+    if (this.filterForm.value.precio > 0) {
+      this.listaMostrar = this.listaMostrar.filter(e => {
+        return e.precio <= this.filterForm.value.precio
+      })
+    }
+  }
+
+
+  reset(): void {
+    this.filterForm.patchValue({
+      id: '',
+      nombre: '',
+      precio: '',
+      categoria: '',
+      marca: ''
+    });
+  }
+
+}
